76e320fb10d74b82f8a0f1831c093811e9abc833
bot317 <bot317@yandex.ru>
2024-03-07 18:49:21 +0500

    lesson 16

---
+++ src/test/java/Tests.java
@@ -0,0 +1,31 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+public class Tests {
+    @BeforeAll
+    public static void setup() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testPost() {
+    }
+
+    @Test
+    public void testPut() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testPatchMethod() {
+
+    }
+}
